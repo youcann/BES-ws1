23ca735a723386ded8c970fb8fa6e495b1310b95
Marvin Noll <marvin.noll@googlemail.com>
2019-05-27 09:04:30 +0200

    recent

--- aufgabe2/aufgabe2.m
+++ aufgabe2/aufgabe2.m
@@ -1,4 +1,4 @@
- 
+
 % Ignorieren Sie die folgende Zeile. Überall wo ZuVeraendernderAusdruck im folgenden auftaucht 
 % sind Änderungen durch Sie erforderlich.
 ZuVeraendernderAusdruck=nan;
@@ -30,54 +30,67 @@ nVoltages = length(clampVoltages); % Anzahl an clamp voltages
 nTimesteps = length(timesteps);
 alpha_n = zeros(nVoltages, nTimesteps); 
 beta_n = zeros(nVoltages, nTimesteps); 
-n = zeros(nVoltages, nTimesteps); n(:,1)=n_0*ones(1,nVoltages);
+n = zeros(nVoltages, nTimesteps); 
 ndot = zeros(nVoltages, nTimesteps);
 I_K = zeros(nVoltages, nTimesteps); 
 alpha_m = zeros(nVoltages, nTimesteps); 
 beta_m = zeros(nVoltages, nTimesteps); 
-m = zeros(nVoltages, nTimesteps); m(:,1)=m_0*ones(1,nVoltages); 
+m = zeros(nVoltages, nTimesteps); 
 mdot = zeros(nVoltages, nTimesteps); 
 alpha_h = zeros(nVoltages, nTimesteps); 
 beta_h = zeros(nVoltages, nTimesteps); 
-h = zeros(nVoltages, nTimesteps); h(:,1)=h_0*ones(1,nVoltages);
+h = zeros(nVoltages, nTimesteps); 
 hdot = zeros(nVoltages, nTimesteps); 
-I_Na = zeros(nVoltages, nTimesteps); 
+I_Na = zeros(nVoltages, nTimesteps);
+%Startwerte
+n(:,1)=n_0*ones(1,nVoltages);
+m(:,1)=m_0*ones(1,nVoltages);
+h(:,1)=h_0*ones(1,nVoltages);
+
+Vm=zeros(nVoltages,nTimesteps);
 
 %% Simulation
 % Berechne Raten, Gates (Euler-1-Schritt), I_K und I_Na
 for iVoltage=1:nVoltages
     for iTimestep=1:nTimesteps
         %Vm
-        if (timesteps(iTimestep)<0.3)
-            Vm_current=-65;
-        elseif (timesteps(iTimestep)>0.3 && timesteps(iTimestep)<7.3)
+        if (timesteps(iTimestep)<=0.3)
+            Vm_current=VpreStep;
+        elseif (timesteps(iTimestep)>0.3 && timesteps(iTimestep)<=7.3)
             Vm_current=clampVoltages(iVoltage);    
-        elseif (timesteps(iTimestep)>7.3 && timesteps(iTimestep)<10)
-             Vm_current=-110;   
+        elseif (timesteps(iTimestep)>7.3 && timesteps(iTimestep)<=10)
+             Vm_current=VpostStep;   
         else
              Vm_current=NaN;   
-        end 
+        end
+        Vm(iVoltage,iTimestep)=Vm_current;
         
         % Kalium
-        I_K(iVoltage,iTimestep) = G_K_max * n(iVoltage,iTimestep).^4 * Vm_current;
+        I_K(iVoltage,iTimestep) = G_K_max * n(iVoltage,iTimestep).^4 * (Vm_current-E_K);
 
         % Natrium
-        I_Na(iVoltage,iTimestep) = G_Na_max * m(iVoltage,iTimestep).^3 * h(iVoltage,iTimestep) * Vm_current;
+        I_Na(iVoltage,iTimestep) = G_Na_max * m(iVoltage,iTimestep).^3 * h(iVoltage,iTimestep) * (Vm_current-E_Na);
 
         %Uebergangsraten
         alpha_n(iVoltage,iTimestep) = 0.01*(-(Vm_current+55)/(exp(-(Vm_current+55)/(10))-1));
+        if(isnan(alpha_n(iVoltage,iTimestep))) alpha_n(iVoltage,iTimestep) = 1; end
         beta_n(iVoltage,iTimestep) = 0.125*exp(-(Vm_current+65)/(80));
+        if(isnan(beta_n(iVoltage,iTimestep))) beta_n(iVoltage,iTimestep) = 1; end
      
-        alpha_m(iVoltage,iTimestep) = 0.1*(-(Vm_current+40)/(exp(-(Vm_current+40)/(10))-1));        
+        alpha_m(iVoltage,iTimestep) = 0.1*(-(Vm_current+40)/(exp(-(Vm_current+40)/(10))-1));
+        if(isnan(alpha_m(iVoltage,iTimestep))) alpha_m(iVoltage,iTimestep) = 1; end
         beta_m(iVoltage,iTimestep) = 4*exp(-(Vm_current+65)/(18));
+        if(isnan(beta_m(iVoltage,iTimestep))) beta_m(iVoltage,iTimestep) = 1; end
      
         alpha_h(iVoltage,iTimestep) = 0.07*exp(-(Vm_current+65)/(20));
+        if(isnan(alpha_h(iVoltage,iTimestep))) alpha_h(iVoltage,iTimestep) = 1; end
         beta_h(iVoltage,iTimestep) = 1/(exp(-(Vm_current+35)/(10))+1);
-        
+        if(isnan(beta_h(iVoltage,iTimestep))) beta_h(iVoltage,iTimestep) = 1; end
+
         %nmh dots
         ndot(iVoltage,iTimestep) = alpha_n(iVoltage,iTimestep) .* (1-n(iVoltage,iTimestep)) - beta_n(iVoltage,iTimestep) .* n(iVoltage,iTimestep);
         mdot(iVoltage,iTimestep) = alpha_m(iVoltage,iTimestep) .* (1-m(iVoltage,iTimestep)) - beta_m(iVoltage,iTimestep) .* m(iVoltage,iTimestep);
-        hdot(iVoltage,iTimestep) = alpha_n(iVoltage,iTimestep) .* (1-h(iVoltage,iTimestep)) - beta_h(iVoltage,iTimestep) .* h(iVoltage,iTimestep);
+        hdot(iVoltage,iTimestep) = alpha_h(iVoltage,iTimestep) .* (1-h(iVoltage,iTimestep)) - beta_h(iVoltage,iTimestep) .* h(iVoltage,iTimestep);
         
         if(iTimestep < nTimesteps)
             n(iVoltage,iTimestep+1) = deltat * ndot(iVoltage,iTimestep) + n(iVoltage,iTimestep);
@@ -87,6 +100,7 @@ for iVoltage=1:nVoltages
         
     end
 end
+
 
 %% Plotten
 % Plotten der Raten und Gates für eine Spannung (Index 14) und Plotten der Ströme für alle Spannungen
